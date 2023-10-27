04d4ed11bbd5e77aa17649ce75649946ce54be09
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2023-10-27 13:49:36 +0500

    notifications

--- src/auth/Index.vue
+++ src/auth/Index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="auth_wrapper">
         <div class="auth_content">
-            <div class="auth_title">
+            <div :class="`auth_title ${ $route.name !== 'AuthIn' ?'auth_title_mobile':''}`">
                 <img src="@/assets/images/logo.svg" alt="">
                 <span>E-Wallet</span>
             </div>
