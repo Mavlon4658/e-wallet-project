03ae096e6990aac859c23a2a63d1c44491bb95e0
Mavlon4658 <mavlon4658@gmail.com>
2023-10-29 15:05:19 +0500

    scanner deyarli tayyor

--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -1,17 +1,51 @@
 <template>
     <div class="payment_page">
-        <div class="title">Отправить</div>
-        <WalletList />
-        <input type="file" id="select_file" @change="uploadFile" ref="file">
-        <label :class="darag_enter ? 'active' : ''" for="select_file" @dragenter="darag_enter = true" @dragleave="darag_enter = false" @dragover="handleDragOver" @drop="handleDrop">
-            <img src="@/assets/images/upload_file.svg" alt="">
-            <div class="text">
-                <span>Перетащите или выберите файл QR-code</span>
-                Допустимые форматы: png, jpg
+        <!-- Payent Desktop -->
+        <div class="payment_desktop">
+            <div class="title">Отправить</div>
+            <WalletList />
+            <input type="file" id="select_file" @change="uploadFile" ref="file">
+            <label :class="darag_enter ? 'active' : ''" for="select_file" @dragenter="darag_enter = true" @dragleave="darag_enter = false" @dragover="handleDragOver" @drop="handleDrop">
+                <img src="@/assets/images/upload_file.svg" alt="">
+                <div class="text">
+                    <span>Перетащите или выберите файл QR-code</span>
+                    Допустимые форматы: png, jpg
+                </div>
+                <img src="" alt="" class="bg_img" ref="bg_img">
+            </label>
+            <Button @btnClick="saveFile()" title="Отправить"/>
+        </div>
+
+        <!-- Payment Mobile -->
+        <div class="payment_mobile">
+            <div :class="`head_buttons_bg ${scanner_open ? 'active' : ''}`"></div>
+            <div class="head_buttons">
+                <span :class="scanner_open ? 'active' : ''"></span>
+                <button @click="scanner_open = false" :class="!scanner_open ? 'active' : ''">Ваш QR-code</button>
+                <button @click="scanner_open = true" :class="scanner_open ? 'active' : ''">Сканировать</button>
             </div>
-            <img src="" alt="" class="bg_img" ref="bg_img">
-        </label>
-        <Button @btnClick="saveFile()" title="Отправить"/>
+            <!-- Your QR-code -->
+            <div :class="`your_qr-code ${!scanner_open ? 'active' : ''}`">
+                <div class="bg_fon">
+                    <img src="@/assets/images/your_qr_code_fon.svg" alt="">
+                </div>
+                <div class="imgs">
+                    <img src="@/assets/images/user_qr_code.png" alt="" class="user_qr_code">
+                    <img src="@/assets/images/avatar.svg" alt="" class="user_logotip">
+                </div>
+                <div class="title">Это ваш  QR-code</div>
+                <div class="sub_title">Поделитесь кодом, чтобы получать оплату</div>
+                <button class="link">
+                    <span>Скачать QR-code</span>
+                    <img src="@/assets/images/upload_file_icon.svg" alt="">
+                </button>
+            </div>
+
+            <!-- Scanner -->
+            <div :class="`scanner ${scanner_open ? 'active' : ''}`">
+                Hello scanner
+            </div>
+        </div>
     </div>
 </template>
 
@@ -27,7 +61,8 @@ export default {
     },
     data () {
         return {
-            darag_enter: false
+            darag_enter: false,
+            scanner_open: false
         }
     },
     methods: {
